a9402326d9bb1b5cf15fe008386f894f4d9ac625
Aligern <sasso.luca93@gmail.com>
2024-03-14 12:41:46 +0100

    second snack

--- N array/js/script.js
+++ N array/js/script.js
@@ -1,5 +1,30 @@
 // Snack3
-//    Fai inserire un numero, che chiameremo N, all'utente.
-//    Genera con una funzione N array, ognuno formato da 10 numeri casuali da 1 a 100.
-//    Ogni volta che ne crei uno, stampalo.
+// we ask the user an N number
+// we generate N arrays with a function, each array need to have 10 random numbers between 1 and 100.
 
+// every time we create one we print it:
+
+
+// we ask the user our N number:
+let N = parseInt(prompt('inserisci un numero'));
+
+// this for loop generates N arrays:
+for (let i = 0; i < N; i++) {
+    const result = createArray ();
+    console.log(createArray());
+}
+
+// this function generate the random numbers inside our arrays:
+function createArray () {
+    let newArray = [];
+    for (i = 0; i <= 10; i++) {
+        newArray.push(getRndInteger(1,100));
+    }
+    return newArray;
+};
+
+
+// this function generate random numbers:
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+};
